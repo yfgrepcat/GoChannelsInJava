45363e834223b86e6313c6d6fc27c9377294e720
Yoann FRANCOIS <121094049+yfrancois-iut@users.noreply.github.com>
2026-05-11 06:45:13 +0200

    feat: Concurrent factory with atomic creation of channels (simultaneous read and write)

--- go/cs/Factory.java
+++ go/cs/Factory.java
@@ -2,25 +2,27 @@ package go.cs;
 
 import go.Direction;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.Map;
+import java.util.HashMap;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Factory implements go.Factory {
 
+    private final ConcurrentHashMap<String, go.Channel> channels = new ConcurrentHashMap<>();
     /** Création ou accès à un canal existant.
      * Côté serveur, le canal est créé au premier appel avec un nom donné ;
      * les appels suivants avec le même nom donneront accès au même canal.
      */
     public <T> go.Channel<T> newChannel(String name) {
-        // TODO
-        return null;
+        // Pas de blocage en lecture, alternative à HashMap + synchronized
+        return channels.computeIfAbsent(name, k -> new Channel<T>(k));
     }
     
     /** Spécifie quels sont les canaux écoutés et la direction pour chacun. */
     public go.Selector newSelector(Map<go.Channel, Direction> channels) {
-        // TODO
-        return null;
+        return new Selector(channels);
     }
 
     /** Spécifie quels sont les canaux écoutés et la même direction pour tous. */
